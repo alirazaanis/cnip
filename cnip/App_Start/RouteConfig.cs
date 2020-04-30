a6ce4afa8055a814e73d6ca1ea3078ed0227d2e9
Ali Raza Anis <alirazaanis@gmail.com>
2020-04-30 22:58:51 +0500

    latest

--- cnip/App_Start/RouteConfig.cs
+++ cnip/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace cnip
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Glavni", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
